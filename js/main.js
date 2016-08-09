05fb212011aef24e223f2c86e1fae622cfdf9827
ljlijing <2636821134@qq.com>
2016-08-09 22:22:59 +0800

    上传

---
+++ js/main.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function(){
+	$(".tecPart_nav > li").hover(function(){
+		$(this).find("ul").css("display","block");
+	},function(){
+		$(this).find("ul").css("display","none");
+	});
+});
